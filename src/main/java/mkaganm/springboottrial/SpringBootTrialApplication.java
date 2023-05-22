220136f726ba496682bfb1479779b7c28c1ae2c2
kagan.meric <mustafakaganmeric@gmail.com>
2023-05-22 04:47:09 +0300

    add SpringBootTrialApplication

--- src/main/java/mkaganm/springboottrial/SpringBootTrialApplication.java
+++ src/main/java/mkaganm/springboottrial/SpringBootTrialApplication.java
@@ -3,6 +3,7 @@ package mkaganm.springboottrial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
