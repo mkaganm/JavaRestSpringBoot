72d05389179741c583f6a49aabf3fbb2cbd0150a
kagan.meric <mustafakaganmeric@gmail.com>
2023-05-22 04:46:16 +0300

    add EmployeeEntity

---
+++ src/main/java/mkaganm/springboottrial/data/entity/EmployeeEntity.java
@@ -0,0 +1,39 @@
+package mkaganm.springboottrial.data.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@Builder
+@Log4j2
+
+@Entity
+@Table(name = "empyloyees")
+public class EmployeeEntity extends BaseEntity implements Serializable {
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public EmployeeEntity(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+}
